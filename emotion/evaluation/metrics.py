357a5d32aa221c92b87de7afed6acfb548180abe
Tushar Dhyani <dhyanitushar@gmail.com>
2021-05-15 01:45:25 +0200

    Added file reader util file

--- emotion/evaluation/metrics.py
+++ emotion/evaluation/metrics.py
@@ -1,26 +1,11 @@
-class Eval:
-    def __init__(self, y_true: list[str, int], y_pred: list[str, int]) -> None:
-        """Evaluation class to calculate the scores of prediction of the entire batch.
-
-        Args:
-            y_true (list[str, int]): List of list containing the predictions in either string or int format
-            y_pred (list[str, int]): List of list of predicted labels containing either string or int of the target labels.
-        """
-        self.y_true = y_true
-        self.y_pred = y_pred
-
-    def get_tp(self):
-        pass
-
-
-def jaccard_score(y_true: list[str, int], y_pred: list[str, int]):
+def jaccard_score(y_true, y_pred):
     """Calculate the jaccard score of the provided sequence slice
     Jaccard index = $\frac{Intersection of argument list}{Union of argument list}$
 
     Ref: https://en.wikipedia.org/wiki/Jaccard_index
     Args:
-        y_true (list[str, int]): Sequence slice gold labels
-        y_pred (list[str, int]): Sequence slice predicted labels
+        y_true (List[str, int]): Sequence slice gold labels
+        y_pred (List[str, int]): Sequence slice predicted labels
     """
     s1 = set(y_true)
     s2 = set(y_pred)
@@ -28,12 +13,12 @@ def jaccard_score(y_true: list[str, int], y_pred: list[str, int]):
     return ji
 
 
-def tp(y_true: list[int], y_pred: list[int]):
+def tp(y_true, y_pred):
     """Python function to calculate values of True Positives
 
     Args:
-        y_true (list[int]): List of int containing the gold labels
-        y_pred (list[int]): List of int containing the predicted labels
+        y_true (List[int]): List of int containing the gold labels
+        y_pred (List[int]): List of int containing the predicted labels
     """
     tpv = 0
     for gld, prd in zip(y_true, y_pred):
@@ -42,12 +27,12 @@ def tp(y_true: list[int], y_pred: list[int]):
     return tpv
 
 
-def fp(y_true: list[int], y_pred: list[int]):
+def fp(y_true, y_pred):
     """Python function for calculating False positives
 
     Args:
-        y_true (list[int]): List of int containing the gold labels
-        y_pred (list[int]): List of int containing predicted labels
+        y_true (List[int]): List of int containing the gold labels
+        y_pred (List[int]): List of int containing predicted labels
     """
     fpv = 0
     for gld, prd in zip(y_true, y_pred):
@@ -56,12 +41,12 @@ def fp(y_true: list[int], y_pred: list[int]):
     return fpv
 
 
-def tn(y_true: list[int], y_pred: list[int]):
+def tn(y_true, y_pred):
     """Python function for calculating True negatives
 
     Args:
-        y_true (list[int]): List of int containing the gold labels
-        y_pred (list[int]): List of int containing predicted labels
+        y_true (List[int]): List of int containing the gold labels
+        y_pred (List[int]): List of int containing predicted labels
     """
     tnv = 0
     for gld, prd in zip(y_true, y_pred):
@@ -70,12 +55,12 @@ def tn(y_true: list[int], y_pred: list[int]):
     return tnv
 
 
-def fn(y_true: list[int], y_pred: list[int]):
+def fn(y_true, y_pred):
     """Python function for calculating False negatives
 
     Args:
-        y_true (list[int]): List of int containing the gold labels
-        y_pred (list[int]): List of int containing predicted labels
+        y_true (List[int]): List of int containing the gold labels
+        y_pred (List[int]): List of int containing predicted labels
     """
     fnv = 0
     for gld, prd in zip(y_true, y_pred):
@@ -84,12 +69,12 @@ def fn(y_true: list[int], y_pred: list[int]):
     return fnv
 
 
-def precision(y_true: list[int], y_pred: list[int]):
+def precision(y_true, y_pred):
     """Python function for calculating precision
 
     Args:
-        y_true (list[int]): List of int containing the gold labels
-        y_pred (list[int]): List of int containing predicted labels
+        y_true (List[int]): List of int containing the gold labels
+        y_pred (List[int]): List of int containing predicted labels
     """
     tpv = tp(y_true, y_pred)
     fpv = fp(y_true, y_pred)
@@ -97,12 +82,12 @@ def precision(y_true: list[int], y_pred: list[int]):
     return prec
 
 
-def recall(y_true: list[int], y_pred: list[int]):
+def recall(y_true, y_pred):
     """Python function for calculating recall
 
     Args:
-        y_true (list[int]): List of int containing the gold labels
-        y_pred (list[int]): List of int containing predicted labels
+        y_true (List[int]): List of int containing the gold labels
+        y_pred (List[int]): List of int containing predicted labels
     """
     tpv = tp(y_true, y_pred)
     fnv = fn(y_true, y_pred)
@@ -110,12 +95,12 @@ def recall(y_true: list[int], y_pred: list[int]):
     return recll
 
 
-def fscore(y_true: list[int], y_pred: list[int], beta: int = 1):
+def fscore(y_true, y_pred, beta: int = 1):
     """Python function for calculating recall
     Ref: https://en.wikipedia.org/wiki/F-score
     Args:
-        y_true (list[int]): List of int containing the gold labels
-        y_pred (list[int]): List of int containing predicted labels
+        y_true (List[int]): List of int containing the gold labels
+        y_pred (List[int]): List of int containing predicted labels
         beta (int, optional): [description]. Defaults to 1.
     """
     pv = precision(y_true, y_pred)
